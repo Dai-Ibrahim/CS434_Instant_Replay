f122c1b0d48d019da707882743ecc39aaf8c3cca
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-02-08 18:09:44 -0700

    instant replay resubmission

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject completeLevelUI;
+    bool instantReplay = false;
+    GameObject player;
+    float replayStartTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerMovement playerMovement = FindObjectOfType<PlayerMovement>();
+        player = playerMovement.gameObject;
+
+        if (CommandLog.commands.Count > 0)
+        {
+            instantReplay = true;
+            replayStartTime = Time.timeSinceLevelLoad;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (instantReplay)
+        {
+            RunInstantReplay();
+        }
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", 2f);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void RunInstantReplay()
+    {
+        if (CommandLog.commands.Count == 0)
+        {
+            return;
+        }
+
+        Command command = CommandLog.commands.Peek();
+        if (Time.timeSinceLevelLoad >= command.timestamp) // + replayStartTime)
+        {
+            command = CommandLog.commands.Dequeue();
+            command.player_rb = player.GetComponent<Rigidbody>();
+            Invoker invoker = new Invoker();
+            invoker.disableLog = true;
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+        }
+    }
+}
---
+++ Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿//This script handles the scoring system of the game.
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	//Define public variables
+	public Transform player;
+	public Text scoreText;
+//	public Text CommandText;
+	//Command command;
+
+	
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0"); //As the player travels further on the z axis, their score will increase.
+		//if(CommandLog.commands.Count > 0)
+		//{
+			//Debug.Log(CommandLog.commands.Count);
+		//	command = CommandLog.commands.Dequeue();
+		//	CommandText.text = command.ToString();
+//			Debug.Log(ToString(command));
+		//	Debug.Log(CommandLog.commands.Count);
+		//}
+		
+    }
+}
